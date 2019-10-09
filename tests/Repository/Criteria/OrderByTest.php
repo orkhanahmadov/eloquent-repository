6bdf15a770ba605568e0d9851bc4954603899032
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-10-09 16:08:17 +0200

    OrderBy criteria test overwritten

---
+++ tests/Repository/Criteria/OrderByTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Orkhanahmadov\EloquentRepository\Tests\Repository\Criteria;
+
+use Illuminate\Database\Eloquent\Builder;
+use Orkhanahmadov\EloquentRepository\EloquentRepository;
+use Orkhanahmadov\EloquentRepository\Repository\Eloquent\Criteria\OrderBy;
+use Orkhanahmadov\EloquentRepository\Tests\fixtures\ModelOne;
+use Orkhanahmadov\EloquentRepository\Tests\TestCase;
+
+class OrderByTest extends TestCase
+{
+    public function testOrderByCriterion()
+    {
+        $repositoryClass = new \ReflectionClass(EloquentRepository::class);
+        $modelInstanceProp = $repositoryClass->getProperty('modelInstance');
+        $modelInstanceProp->setAccessible(true);
+        $builderClass = new \ReflectionClass(Builder::class);
+        $queryProp = $builderClass->getProperty('query');
+        $queryProp->setAccessible(true);
+
+        $result = $this->repository
+            ->entity(ModelOne::class)
+            ->withCriteria(new OrderBy('someColumnName', 'desc'));
+
+        $ordering = $queryProp->getValue($modelInstanceProp->getValue($result))->orders[0];
+        $this->assertSame('someColumnName', $ordering['column']);
+        $this->assertSame('desc', $ordering['direction']);
+    }
+}
